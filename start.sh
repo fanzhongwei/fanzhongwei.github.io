40528c4e4d3bf2d94ad121dfaf5486cfabc39fc0
fanzhongwei <fanzhongwei@thunisoft.com>
2019-04-11 20:52:26 +0800

    修改启动停止脚本

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nohup hexo server -p 80 >> blog.out 2>&1 $
+nohup hexo server -p 80 > blog.out 2>&1 &
--- stop.sh
+++ stop.sh
@@ -2,14 +2,14 @@
 APP_NAME=hexo
 
 tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
-if [[ ${tpid} ]];
+if [ -n "$tpid" ];
 then
     echo 'Stop Process...'
     kill -15 $tpid
 fi
 sleep 5
 tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
-if [[ ${tpid} ]];
+if [ -n "$tpid" ];
 then
     echo 'Kill Process!'
     kill -9 $tpid
