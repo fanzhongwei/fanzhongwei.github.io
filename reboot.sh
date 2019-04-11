f187e22dac90aff02189a94492f6d5f5f85878d7
fanzhongwei <fanzhongwei@thunisoft.com>
2019-04-11 21:02:39 +0800

    重启脚本添加注释

--- reboot.sh
+++ reboot.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo 'Update remote code!'
+
 git fetch --all  
 git reset --hard origin/master 
 git pull
@@ -10,18 +12,21 @@ APP_NAME=hexo
 tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
 if [ -n "$tpid" ];
 then
-    echo 'Stop Process...'
+    echo 'Stop hexo server Process...'
     kill -15 $tpid
 fi
 sleep 5
 tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
 if [ -n "$tpid" ];
 then
-    echo 'Kill Process!'
+    echo 'Kill hexo server Process!'
     kill -9 $tpid
 else
-    echo 'Stop Success!'
+    echo 'Stop hexo server Success!'
 fi
 
+echo 'Start hexo server and listening 80 port...'
 
 nohup hexo server -p 80 > blog.out 2>&1 &
+
+echo 'Hexo server is started!'
