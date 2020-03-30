179ef0ae428c0c560d7cf56e3bd3a50147ba65ff
teddy <fzwtingyu@163.com>
2020-03-30 23:37:37 +0800

    修改重新部署脚本

--- redeploy.sh
+++ redeploy.sh
@@ -3,11 +3,11 @@
 echo 'Update remote code!'
 
 git fetch --all  
-git reset --hard origin/master 
+git reset --hard origin/develop 
 git pull
 
 hexo g
 
 hexo deploy
 
-echo 'Hexo server is started!'
+echo 'Hexo server is deployed!'
